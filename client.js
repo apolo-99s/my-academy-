5d1ca745a80d83a80adbd2460d804996ec990eaf
apolo-99s <fycaltoxic@gmail.com>
2026-07-23 18:58:21 +0100

    Update client.js

--- client.js
+++ client.js
@@ -4,5 +4,5 @@
 // Requires the Supabase JS CDN script to be loaded first.
 // ============================================================
 var SUPABASE_URL = 'https://myrhxpctkbxkfkrxtjfs.supabase.co';
-var SUPABASE_KEY = 'sb_publishable_7eV-URskWryaWzbLQENLbQ_80KDVKRX';
+var SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ2cWt4dGllcGd4dnltaHRsbHdhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ3NDQ0NDgsImV4cCI6MjEwMDMyMDQ0OH0.oaY-_zDZDFVuIuv4szLyJRZ8cnfDyOlg7mbPtRG0yTE';
 var sb = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
